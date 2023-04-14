7f4dc8a90c82849c3b409244eb98a57f65b59621
Hong Ky <vuonghongky26@gmail.com>
2023-04-14 19:13:48 +0700

    Install: Template engine (handlebars) & SCSS

--- block/src/index.js
+++ block/src/index.js
@@ -1,8 +1,11 @@
 const morgan = require("morgan");
+const path = require("path");
 const express = require("express");
 const handlebars = require("express-handlebars").engine;
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -15,7 +18,7 @@ app.engine(
     })
 );
 app.set("view engine", "hbs");
-app.set("views", "./src/resources/views");
+app.set("views", path.join(__dirname, "resources/views"));
 
 app.get("/", (req, res) => {
     res.render("home");
